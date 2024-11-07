925c2a6b24479ea3fdbb9ee958a98b1f062c6eb0
Irshath Nagouri <irsnag@gmail.com>
2024-11-07 19:42:11 +0100

    update test

--- test_package.py
+++ test_package.py
@@ -33,3 +33,52 @@ for k in range(1, num_steps + 1):
 from stonesoup.plotter import Plotter
 plotter = Plotter()
 plotter.plot_ground_truths(truth, [0, 2])
+
+print(transition_model.matrix(time_interval=timedelta(seconds=0.5)))
+
+
+from stonesoup.types.detection import Detection
+from stonesoup.models.measurement.linear import LinearGaussian
+
+measurement_model = LinearGaussian(
+    ndim_state=4,  # Number of state dimensions (position and velocity in 2D)
+    mapping=(0, 2),  # Mapping measurement vector index to state index
+    noise_covar=np.array([[5, 0],  # Covariance matrix for Gaussian PDF
+                          [0, 5]])
+    )
+
+print(measurement_model.matrix())
+print(measurement_model.covar())
+
+measurements = []
+for state in truth:
+    measurement = measurement_model.function(state, noise=True)
+    measurements.append(Detection(measurement,
+                                  timestamp=state.timestamp,
+                                  measurement_model=measurement_model))
+    
+plotter.plot_measurements(measurements, [0, 2])
+plotter.fig
+
+from stonesoup.predictor.kalman import KalmanPredictor
+predictor = KalmanPredictor(transition_model)
+
+from stonesoup.updater.kalman import KalmanUpdater
+updater = KalmanUpdater(measurement_model)
+
+from stonesoup.types.state import GaussianState
+prior = GaussianState([[0], [1], [0], [1]], np.diag([1.5, 0.5, 1.5, 0.5]), timestamp=start_time)
+
+from stonesoup.types.hypothesis import SingleHypothesis
+
+from stonesoup.types.track import Track
+track = Track()
+for measurement in measurements:
+    prediction = predictor.predict(prior, timestamp=measurement.timestamp)
+    hypothesis = SingleHypothesis(prediction, measurement)  # Group a prediction and measurement
+    post = updater.update(hypothesis)
+    track.append(post)
+    prior = track[-1]
+
+plotter.plot_tracks(track, [0, 2], uncertainty=True)
+plotter.fig
